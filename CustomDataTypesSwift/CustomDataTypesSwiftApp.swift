db0278a17c0bb7c79fd218c212fc9c2808622714
Nikita12G <gulyi08@mail.ru>
2024-07-19 15:42:30 +0700

    Initial Commit

---
+++ CustomDataTypesSwift/CustomDataTypesSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomDataTypesSwiftApp.swift
+//  CustomDataTypesSwift
+//
+//  Created by Никита Гуляев on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomDataTypesSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
